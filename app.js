731a2e105644608855e744f6ae1428889bc99988
stefko-tsonyovski <116901971+stefko-tsonyovski@users.noreply.github.com>
2022-12-14 14:56:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(xss());
 app.get("/", (req, res) => {
   res.send(`<h1>Jobs API</h1><a href="/api-docs">Documentation</a>`);
 });
-app.use("/api-use", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 const authRouter = require("./routes/auth");
